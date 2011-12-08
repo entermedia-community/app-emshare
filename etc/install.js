1905197b1c9821084afda9594b0925929e34f949
cburkey <cburkey@entermediasoftware.com>
2011-12-08 13:56:50 -0500

    Fix link

--- etc/install.js
+++ etc/install.js
@@ -4,6 +4,7 @@ importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
 var appname = "app-emshare";
+var foldername = "emshare";
 
 //http://dev.entermediasoftware.com/jenkins/job/app-emshare/lastSuccessfulBuild/artifact/deploy/app-emshare.zip
 var zip = "http://dev.entermediasoftware.com/jenkins/job/" + appname + "/lastSuccessfulBuild/artifact/deploy/" + appname + ".zip";
@@ -21,8 +22,8 @@ unziper.unzip(  tmp + "/" + appname + ".zip",  tmp );
 
 var files = new FileUtils();
 log.add("3. UPGRADE BASE DIR");
-files.deleteAll( root + "/WEB-INF/base" + appname);
-files.copyFiles( tmp + "/" + appname, root + "/WEB-INF/base" + appname + "/");
+files.deleteAll( root + "/WEB-INF/base" + foldername);
+files.copyFiles( tmp + "/" + foldername, root + "/WEB-INF/base/" + foldername + "/");
 
 log.add("4. CLEAN UP");
 files.deleteAll(tmp);
