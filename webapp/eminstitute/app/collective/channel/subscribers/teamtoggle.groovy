483a4acd04626235397d4cc5a120888a81d53f90
cburkey <cburkey@entermediadb.org>
2019-05-13 17:25:08 -0400

    Added subscribe

---
+++ webapp/eminstitute/app/collective/channel/subscribers/teamtoggle.groovy
@@ -0,0 +1,32 @@
+package subscribers;
+
+import org.entermediadb.asset.Category
+import org.entermediadb.asset.MediaArchive
+import org.openedit.Data
+import org.openedit.data.Searcher
+
+
+public void init()
+{
+
+	String collectionid = context.getRequestParameter("collectionid");
+	String userid = context.getRequestParameter("userid");
+	MediaArchive archive = context.getPageValue("mediaarchive");
+	Data subscription = archive.query("collectivesubscribers").exact("collectionid",collectionid).exact("followeruser",userid).searchOne();
+	//log.info(collectionid  + " and " + userid + " = " + subscription);
+	if(subscription != null)
+	{
+		Boolean onteam = subscription.getBoolean("ontheteam")
+		onteam = !onteam;
+		subscription.setValue("ontheteam",onteam);
+		archive.saveData("collectivesubscribers",subscription);
+		
+		context.putPageValue("subscription",subscription);
+	}	
+	//context.redirect("/"+$applicationid+"/collective/channel/");
+		
+}
+
+init();
+
+
