41bf699e5415d2f9e17b6bff5b0384c6ce51035e
cburkey <cburkey@entermediadb.org>
2019-05-20 18:25:21 -0400

    Comments

---
+++ webapp/eminstitute/app/collective/channel/comments/comments.js
@@ -0,0 +1,35 @@
+initcomments = function()
+{
+	var app = $("#application");
+	var apphome = app.data("home") + app.data("apphome");
+
+	lQuery('input.commentadder').livequery("keyup",function(e) 
+	{
+		//Listen for enter
+		var input = $(this);
+		var div = input.closest(".feedcard-comments");
+		var code = e.which; // recommended to use e.which, it's normalized across browsers
+	    if(code==13)e.preventDefault();
+	    if(code==32||code==13||code==188||code==186)
+	    {
+	     	//Submit repaint
+	     	var options = input.data();
+	     	options.oemaxlevel = 1;
+	     	options.commenttext = input.val();
+	     	var link = apphome + "/collective/channel/comments/addcomment.html";
+			jQuery.get(link,options,
+				function(data) 
+				{
+					div.replaceWith(data);
+				}
+	        );	     	
+	    } // missing closing if brace 
+		
+	});
+		
+}
+
+$(document).ready(function() {
+
+	initcomments();
+});
