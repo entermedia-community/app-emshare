886fff9f92b3bf41785035b627322c8e297129cf
Cristobal <cristobal@entermediasoftware.com>
2019-11-05 16:08:22 -0600

    Add to team script

--- webapp/eminstitute/app/collective/channel/subscribers/addtoteam.groovy
+++ webapp/eminstitute/app/collective/channel/subscribers/addtoteam.groovy
@@ -2,6 +2,7 @@ package subscribers;
 
 import org.entermediadb.asset.Category
 import org.entermediadb.asset.MediaArchive
+import org.entermediadb.email.WebEmail
 import org.openedit.Data
 import org.openedit.data.Searcher
 import org.openedit.users.User
@@ -11,9 +12,9 @@ public void init()
 {
 
 	String collectionid = context.getRequestParameter("collectionid");
-	String firstName = context.getRequestParameter("firstName");
-	String lastName = context.getRequestParameter("lastName");
-	String email = context.getRequestParameter("email");
+	String firstName = context.getRequestParameter("firstName.value");
+	String lastName = context.getRequestParameter("lastName.value");
+	String email = context.getRequestParameter("email.value");
 	
 	
 	MediaArchive archive = context.getPageValue("mediaarchive");
@@ -29,14 +30,32 @@ public void init()
 		archive.getUserManager().saveUser(teamuser);
 	}
 	
-	Data subscription = archive.getSearcher("librarycollectionusers").createNewData();
-	
-	subscription.setValue("collectionid",collectionid);
-	subscription.setValue("followeruser",teamuser.getId());
-	subscription.setValue("ontheteam",true);
-	subscription.setValue("addeddate",new Date());
-	archive.getSearcher("librarycollectionusers").saveData(subscription);
+	Data subscription = archive.query("librarycollectionusers").exact("followeruser", teamuser.getId()).searchOne();
+	if (subscription == null)
+	{
+		subscription = archive.getSearcher("librarycollectionusers").createNewData();
+		subscription.setValue("collectionid",collectionid);
+		subscription.setValue("followeruser",teamuser.getId());
+		subscription.setValue("ontheteam",true);
+		subscription.setValue("addeddate",new Date());
+		archive.getSearcher("librarycollectionusers").saveData(subscription);
+	}
 	context.putPageValue("subscription",subscription);
+	
+	//collection-count-modifed
+	String template = context.findValue("apphome") + "/theme/emails/collection-add-teammember.html";
+
+	WebEmail templatemail = archive.createSystemEmail(teamuser, template);
+	templatemail.setSubject("Added to Team"); //TODO: Translate
+	Map objects = new HashMap();
+	String entermediakey = archive.userManager.getEnterMediaKey(teamuser);
+	objects.put("entermediakey",entermediakey);
+	objects.put("user", context.getUser());
+	objects.put("teamuser",teamuser);
+	Data librarycol = archive.getData("librarycollection", collectionid);
+	objects.put("librarycol", librarycol);
+	objects.put("apphome", context.findValue("apphome"));
+	templatemail.send(objects);
 		
 }
 
