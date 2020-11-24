f0259188e0c9dad13305a372049d355db347d013
Christopher Burkey <cburkey@entermediadb.org>
2020-11-24 13:44:04 -0600

    Rought it in

---
+++ webapp/finder/find/components/upload/entities/picked.groovy
@@ -0,0 +1,44 @@
+import org.openedit.Data
+import org.openedit.MultiValued
+import org.openedit.profile.UserProfile
+
+public void init()
+{
+	UserProfile userprofile = context.getUserProfile();
+	
+	String entitytype = context.getRequestParameter("entitytype");
+	
+	String addselection = context.getRequestParameter("addselection");
+	if( addselection != null)
+	{
+		userprofile.addValue("picked" + entitytype,addselection);
+	}
+
+	String removeselection = context.getRequestParameter("removeselection");
+	if( removeselection != null)
+	{
+		userprofile.removeValue("picked" + entitytype,removeselection);
+	}
+
+}
+
+
+protected String toString(Collection<Data> inValues)
+{
+	StringBuffer values = new StringBuffer();
+	for (Iterator iterator = inValues.iterator(); iterator.hasNext();)
+	{
+		String detail = (String) iterator.next();
+		values.append(detail);
+		if (iterator.hasNext())
+		{
+			values.append(" | ");
+		}
+	}
+	return values.toString();
+}
+
+init();
+
+
+
