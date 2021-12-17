2cd9b5165a948dbf7047c71c2c4384a1695bbbb7
Cristobal Mejia <cristobal@openedit.org>
2021-12-17 16:57:37 -0600

    Donate email confirmation

---
+++ webapp/eminstitute/catalog/events/scripts/billing/donationreceipt.groovy
@@ -0,0 +1,51 @@
+package billing;
+
+import org.entermedia.sitemanager.StripePaymentProcessor
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.email.WebEmail
+import org.openedit.*
+import org.openedit.data.Searcher
+import org.openedit.users.User
+
+public void init() {
+	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
+	Searcher transactionSearcher = mediaArchive .getSearcher("transaction");
+	
+	Data payment = context.getPageValue("payment");
+	Data receipt = context.getPageValue("receipt");
+	
+	User user = context.getUser();
+	
+	String appid = mediaArchive.getCatalogSettingValue("events_billing_notify_invoice_appid");
+	String emailbody = "Thank you for your donation.";
+	String subject = "Donation Receipt";
+	
+	//get emailbody from collection
+	Data collection = mediaArchive.getData("librarycollection", context.getPageValue("collectionid"));
+	if (collection.getValue("donationemailtemplate")) {
+		emailbody = collection.getValue("donationemailtemplate");
+	}
+	
+	context.putPageValue("receiptuser", user);
+	context.putPageValue("mediaarchive", mediaArchive);
+	context.putPageValue("receipt", receipt);
+	context.putPageValue("payment", payment);
+
+	WebEmail templateEmail = mediaArchive.createSystemEmailBody(user, emailbody);
+	templateEmail.setSubject(subject);
+	templateEmail.loadSettings(context);
+	templateEmail.send();
+	
+	log.info("Email sent to: "+user.getEmail());
+}
+
+private String getSiteRoot() {
+	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
+	String site = mediaArchive.getCatalogSettingValue("siteroot");
+	if (!site) {
+		site = mediaArchive.getCatalogSettingValue("cdn_prefix");
+	}
+	return site;
+}
+
+init();
