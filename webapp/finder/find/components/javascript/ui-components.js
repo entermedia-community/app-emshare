b82e62e0b6b299c21c15c9223c6b1a9b0d03c862
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-02-28 13:11:20 -0600

    multiple emdialog z-index

--- webapp/finder/find/components/javascript/ui-components.js
+++ webapp/finder/find/components/javascript/ui-components.js
@@ -1208,7 +1208,26 @@ uiload = function() {
 			modaldialog.modal("hide");
 			modaldialog.remove();
 		}
-	}
+	};
+	
+	$(document).on({
+	    'show.bs.modal': function () {
+	        var zIndex = 1040 + (10 * $('.modal:visible').length);
+	        $(this).css('z-index', zIndex);
+	        setTimeout(function() {
+	            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+	        }, 0);
+	    },
+	    'hidden.bs.modal': function() {
+	        if ($('.modal:visible').length > 0) {
+	            // restore the modal-open class to the body element, so that scrolling works
+	            // properly after de-stacking a modal.
+	            setTimeout(function() {
+	                $(document.body).addClass('modal-open');
+	            }, 0);
+	        }
+	    }
+	}, '.modal');
 	
 	lQuery("a.entity-tab-label").livequery("click", function(event) {
 		event.preventDefault();
