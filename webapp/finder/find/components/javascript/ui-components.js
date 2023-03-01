9e63479e7b4105619f711d3425622170bea0f352
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-02-28 18:54:53 -0600

    modal

--- webapp/finder/find/components/javascript/ui-components.js
+++ webapp/finder/find/components/javascript/ui-components.js
@@ -1212,7 +1212,7 @@ uiload = function() {
 	
 	$(document).on({
 	    'show.bs.modal': function () {
-	        var zIndex = 1040 + (10 * $('.modal:visible').length);
+	        var zIndex = 100000 + (10 * $('.modal:visible').length);
 	        $(this).css('z-index', zIndex);
 	        setTimeout(function() {
 	            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
