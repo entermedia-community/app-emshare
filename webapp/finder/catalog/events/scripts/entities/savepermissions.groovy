3161185b7f70d15e55c2ab4c26ba281246f6693e
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-03-23 10:42:49 -0600

    saving permissions entities

---
+++ webapp/finder/catalog/events/scripts/entities/savepermissions.groovy
@@ -0,0 +1,46 @@
+package entities;
+
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.users.PermissionManager
+import org.openedit.Data
+import org.openedit.data.Searcher
+
+public void init()
+{
+	MediaArchive mediaarchive = (MediaArchive) context.getPageValue("mediaarchive");
+
+	String moduleid = context.getRequestParameter("moduleid");
+	String groupid = context.getRequestParameter("settingsgroupid");
+	
+	PermissionManager permissionManager = mediaarchive.getBean("permissionManager");
+	Map permissionassigned = permissionManager.loadEntitySettingsGroupPermissions(moduleid, groupid);
+	
+	String[] fields = context.getRequestParameters("field");
+	
+	Searcher permissionsSearcher = mediaarchive.getSearcher("permissionentityassigned");
+
+		for (permissionid in fields) {
+		String permissionidvalue = context.getRequestParameter(permissionid+".value");
+		if(permissionid != null && !permissionidvalue.asBoolean() && permissionassigned.containsKey(permissionid))
+		{
+			Data data = permissionassigned.get(permissionid);
+			permissionsSearcher.delete(data, null);
+		}
+		else if(permissionid != null && permissionidvalue.asBoolean() && !permissionassigned.containsKey(permissionid))
+		{
+			Data data = permissionsSearcher.createNewData();
+			data.setValue("moduleid", moduleid);
+			data.setValue("settingsgroup", groupid);
+			data.setValue("permissionsentity", permissionid);
+			data.setValue("enabled", true);
+			permissionsSearcher.saveData(data);
+			
+		}
+	}
+	
+    
+}
+
+
+init();
+
