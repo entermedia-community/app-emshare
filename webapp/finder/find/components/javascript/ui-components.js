8635f16a0a4ce58a46a40a7caf8644e1327552e5
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-05-23 13:35:47 -0600

    Modules/Entities table

--- webapp/finder/find/components/javascript/ui-components.js
+++ webapp/finder/find/components/javascript/ui-components.js
@@ -3386,37 +3386,69 @@ uiload = function() {
 	
 	
 	lQuery('.listsort').livequery(function() {
-	var listtosort = $(this);
-	if (typeof listtosort.sortable == "function") {
-		listtosort.sortable({
-				axis: 'y',
-				cancel: ".no-sort",
-			    stop: function (event, ui) {
-			  
-					var path = $(this).data("path");
-			    	
-			        var data = "";
-	
-			       // var ids = new Array();
-			        $(this).find('li').each(function(index) 
-			        {
-			        	if( !$(this).hasClass("no-sort"))
-			        	{
-			        		var id = $(this).attr("id");
-			        		data = data + "ids=" + id + "&";
-			        	}
-					});
-			        // POST to server using $.post or $.ajax
-			        $.ajax({
-			        	data: data,
-			            type: 'POST',
-			            url: path 		            
-			        });
-			    }
-		});
-	}
+		var listtosort = $(this);
+		if (typeof listtosort.sortable == "function") {
+			listtosort.sortable({
+					axis: 'y',
+					cancel: ".no-sort",
+				    stop: function (event, ui) {
+				  
+						var path = $(this).data("path");
+				    	
+				        var data = "";
+		
+				       // var ids = new Array();
+				        $(this).find('li').each(function(index) 
+				        {
+				        	if( !$(this).hasClass("no-sort"))
+				        	{
+				        		var id = $(this).attr("id");
+				        		data = data + "ids=" + id + "&";
+				        	}
+						});
+				        // POST to server using $.post or $.ajax
+				        $.ajax({
+				        	data: data,
+				            type: 'POST',
+				            url: path 		            
+				        });
+				    }
+			});
+		}
 	});
 		
+
+	lQuery('.tablesort').livequery(function() {
+		var listtosort = $(this);
+		if (typeof listtosort.sortable == "function") {
+			listtosort.sortable({
+					axis: 'y',
+					cancel: ".no-sort",
+				    stop: function (event, ui) {
+				  
+						var path = $(this).data("path");
+				    	
+				        var data = "";
+		
+				       // var ids = new Array();
+				        $(this).find('tr').each(function(index) 
+				        {
+				        	if( !$(this).hasClass("no-sort"))
+				        	{
+				        		var id = $(this).attr("id");
+				        		data = data + "ids=" + id + "&";
+				        	}
+						});
+				        // POST to server using $.post or $.ajax
+				        $.ajax({
+				        	data: data,
+				            type: 'POST',
+				            url: path 		            
+				        });
+				    }
+			});
+		}
+	});
 		
 	lQuery( ".copytoclipboard" ).livequery("click", function(e) {
 		  e.preventDefault();
