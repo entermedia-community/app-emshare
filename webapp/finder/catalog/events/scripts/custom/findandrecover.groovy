c46510d70443265526121bd9584cea550b43f977
Cristobal <cristobal.mejia@gmail.com>
2023-12-21 16:52:57 -0600

    Agbu recovery

--- webapp/finder/catalog/events/scripts/custom/findandrecover.groovy
+++ webapp/finder/catalog/events/scripts/custom/findandrecover.groovy
@@ -22,7 +22,7 @@ public void init()
 	String foundcategory = "AYyDQU31oRsY97PbIb-R"; //Category containing recovered assets
 	
 	Collection foundassets = mediaarchive.query("asset").match("category", foundcategory).search();
-	if( foundassets.size() >= 1) {
+	
 		for(Data asset in foundassets)
 		{
 			String foundmd5 = asset.getValue("md5hex");
@@ -32,20 +32,17 @@ public void init()
 			{
 				for(Data asset2 in duplicatedhits)
 				{
-					ContentItem	 fullpath = 	mediaarchive.getOriginalContent(asset2);
+					ContentItem	destinationfullpath = 	mediaarchive.getOriginalContent(asset2);
 	
-					if(fullpath.getLength() == 138){
+					if(destinationfullpath.getLength() == 138){
 						
-						log.info("Found: "+fullpath + " : " + (String) fullpath.getLength());
-						Page originalpath = pageManager.getPage("/WEB-INF/data/" + mediaarchive.getCatalogId() + "/originals/" + asset2.getSourcePath() );
+						log.info("Found: "+destinationfullpath + " : " + (String) destinationfullpath.getLength());
 						
-						//backup corrupted, for what?
-						Page newpage = pageManager.getPage("/WEB-INF/data/" + mediaarchive.getCatalogId() + "/originals/_corrupted/" + asset2.getSourcePath());
-						pageManager.movePage(originalpath, newpage);
+						ContentItem	sourcefullpath = 	mediaarchive.getOriginalContent(asset);
 						
-						//bring restored file
-						Page restoredpage = pageManager.getPage("/WEB-INF/data/" + mediaarchive.getCatalogId() + "/originals/" + asset.getSourcePath());
-						pageManager.movePage(restoredpage, originalpath);
+						pageManager.getRepository().remove(destinationfullpath);
+						pageManager.getRepository().copy(sourcefullpath, destinationfullpath);
+						
 						
 						//delete restored asset?
 						//mediaarchive.deleteAsset(asset,false);
@@ -55,7 +52,7 @@ public void init()
 					
 				}
 			}
-		}
+		
 	}
 	
 
