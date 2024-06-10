e626b2def6c7f00858834175d07c33258d48ad70
Christopher Burkey <cburkey@entermediadb.org>
2024-06-10 13:08:03 -0600

    Read

--- webapp/finder/catalog/events/scripts/importing/readallmetadata.groovy
+++ webapp/finder/catalog/events/scripts/importing/readallmetadata.groovy
@@ -2,13 +2,9 @@ package importing
 
 import org.entermediadb.asset.Asset
 import org.entermediadb.asset.MediaArchive
-import org.entermediadb.asset.scanner.MetaDataReader
 import org.openedit.Data
 import org.openedit.data.Searcher
 import org.openedit.hittracker.HitTracker
-import org.openedit.repository.ContentItem
-
-import groovy.util.logging.Log
 
 public void init()
 {
@@ -16,52 +12,24 @@ public void init()
 		Searcher searcher = archive.getAssetSearcher();
 		Integer pagesize = 200;
 		
-		HitTracker assets = searcher.query().exact("tempeditstatus","needsmetadata").sort("id").search();
+		HitTracker assets = searcher.query().exact("importstatus","complete").sort("sourcepath").search();
 		assets.enableBulkOperations();
-		assets.setHitsPerPage(pagesize);
-		String ids = context.getRequestParameter("assetids");
-		if( ids != null )
-		{
-			String[] assetids = ids.split(",");
-			assets.setSelections(Arrays.asList( assetids) );
-			assets.setShowOnlySelected(true);
-		}
-
+		
 		List assetsToSave = new ArrayList();
-		MetaDataReader reader = moduleManager.getBean("metaDataReader");
-		log.info("metadatareader assets to read: " + assets.size());
-		Integer count = 0;
 		for (Data hit in assets)
 		{
-			log.info("metadatareader loading asset: ${hit.id}");
 			Asset asset = searcher.loadData(hit);
-			if( asset != null)
+			asset.setValue("importstatus","needsmetadata" );
+			assetsToSave.add(asset);
+			if( assetsToSave.size() == 1000)
 			{
-				count = count +1;
-				ContentItem content = archive.getOriginalContent( asset );
-				log.info("metadatareader asset (${count}) content ${hit.id}: " + content.toString());
-				reader.populateAsset(archive, content, asset);
-				log.info("metadatareader asset populated ${hit.id}");
-				
-				asset.setValue("tempeditstatus", "");
-				
-				assetsToSave.add(asset);
-				if(assetsToSave.size() == pagesize)
-				{
-					log.info("metadatareader about to save 300 assets");
-					archive.saveAssets( assetsToSave );
-					log.info("metadatareader saved "+pagesize+" assets");
-					assetsToSave.clear();
-					count = 0;
-				}
+				searcher.saveAllData(assetsToSave, null);
+				assetsToSave.clear();
 			}
-			else
-			{
-				log.info("metadatareader error loading asset: ${hit.id}");
-			}
-		}
-		archive.saveAssets(assetsToSave);
-		log.info("metadatareader reading complete");
+		}		
+		searcher.saveAllData(assetsToSave, null)
+		archive.fireSharedMediaEvent("importing/assetsreadmetadata");
+		
 }
 
 init();
