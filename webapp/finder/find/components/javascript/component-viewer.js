14366708d283af8ed6c8f34d2bc16a41e6425958
Christopher Burkey <cburkey@entermediadb.org>
2024-08-12 02:03:52 -0600

    Rough render

--- webapp/finder/find/components/javascript/component-viewer.js
+++ webapp/finder/find/components/javascript/component-viewer.js
@@ -1,7 +1,11 @@
+$(document).ready(function () {
+
+
 lQuery("#componentViewer").livequery(function () {
-  var apphome = app.data("siteroot") + app.data("apphome");
+var componentviewer = $(this);
 
   var canvas = new draw2d.Canvas("componentViewer");
+
   canvas.installEditPolicy(
     new draw2d.policy.connection.DragConnectionCreatePolicy({
       createConnection: function () {
@@ -23,204 +27,70 @@ lQuery("#componentViewer").livequery(function () {
   canvas.installEditPolicy(new draw2d.policy.canvas.SnapToGridEditPolicy());
   canvas.installEditPolicy(new draw2d.policy.canvas.CoronaDecorationPolicy());
 
-  var rootId = draw2d.util.UUID.create();
-  var rootJson = function ({ x, y }) {
-    return [
-      {
-        type: "draw2d.shape.composite.Group",
-        id: rootId,
-        x: x,
-        y: y,
-        width: 150,
-        height: 125,
-        bgColor: "none",
-        color: "none",
-        stroke: 0,
-        alpha: 1,
-        cssClass: "rootGroup",
-        ports: [
-          {
-            id: draw2d.util.UUID.create(),
-            type: "draw2d.HybridPort",
-            width: 16,
-            height: 16,
-            selectable: true,
-            draggable: true,
-            bgColor: "#60729e",
-            color: "#4d5d80",
-            stroke: 2,
-            port: "draw2d.HybridPort",
-            locator: "draw2d.layout.locator.XYAbsPortLocator",
-            name: draw2d.util.UUID.create(),
-            locatorAttr: {
-              x: 150,
-              y: 64,
-            },
-          },
-        ],
-      },
-      {
-        type: "draw2d.shape.basic.Image",
-        x: x,
-        y: y,
-        id: draw2d.util.UUID.create(),
-        type: "draw2d.shape.basic.Image",
-        width: 150,
-        height: 125,
-        path: apphome + "/theme/images/ft.svg",
-        draggable: true,
-        selectable: true,
-        resizable: false,
-        cssClass: "rootImage",
-        composite: rootId,
-      },
-      {
-        type: "draw2d.shape.basic.Label",
-        id: rootId + "-label",
-        x: x + 72,
-        y: y + 88,
-        width: 150,
-        text: "Book",
-        textAnchor: "middle",
-        stroke: 0,
-        fontSize: 20,
-        fontWeight: "bold",
-        fontColor: "#ffffff",
-        bgColor: "none",
-        selectable: false,
-        draggable: false,
-        cssClass: "rootLabel",
-        userData: {
-          description: "",
-          moduleid: "",
+
+ function loadJSON() {
+      var url = componentviewer.data("loadurl");
+	  console.log("Loading" + url);
+
+	  var request = {
+		  "componentdatasortby" : "orderingUp",		
+		  "page": "1",
+		  "hitsperpage": "400",
+		  "query": {
+		    "terms": [
+		      {
+		        "field": "entityid",
+		        "operator": "exact",
+		        "value": componentviewer.data("entityid")
+		      }
+		    ]
+		  }
+		};
+	  
+	   var datastring = JSON.stringify(request);
+	  
+      jQuery.ajax({
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        url: url,
+        data: datastring,
+        method: "POST",
+        success: function (res) {
+          if (res.response != undefined && res.response.status == "ok") {
+            var results = res.results;
+            //TODO: Support pagination
+            canvas.clear();
+
+     		var reader = new draw2d.io.json.Reader();
+            for(let i = 0; i < results.length; i++) 
+            {
+			    let data = results[i];
+			    //Fix any more math?
+                
+                //Check for errors
+                try
+                {
+                	var parsed = JSON.parse(data.json);
+	          		reader.unmarshal(canvas, data.json); //?parsed?
+                } catch (e) {
+					
+				  console.log(data.json,e); // Logs the error
+				}
+                
+			}
+              //var updateddata = saveddata.replaceAll("${apphome}", apphome);
+          } else {
+            console.log("Error", res);
+          }
         },
-        composite: rootId,
-      },
-      {
-        type: "draw2d.shape.basic.Image",
-        id: rootId + "-icon",
-        x: x + (150 - 50) / 2,
-        y: y + 30,
-        width: 50,
-        height: 50,
-        draggable: false,
-        selectable: false,
-        composite: rootId,
-        cssClass: "rootIcon",
-        path: apphome + "/theme/icons/bootstrap/folder.svg",
-      },
-    ];
-  };
+      });
+    };
+   componentviewer.on("click", function (e) {
+	loadJSON();
+  });
 
-  var labelPort = {
-    type: "draw2d.HybridPort",
-    bgColor: "#60729e",
-    color: "#4d5d80",
-    port: "draw2d.HybridPort",
-    locator: "draw2d.layout.locator.XYRelPortLocator",
-    width: 16,
-    height: 16,
-  };
-  var componentJson = function (attr) {
-    var componentId = draw2d.util.UUID.create();
-    var data = [
-      {
-        type: "draw2d.shape.composite.Group",
-        id: componentId,
-        x: attr.x,
-        y: attr.y,
-        width: 300,
-        height: 80,
-        bgColor: attr.bgColor,
-        color: attr.color,
-        stroke: 1,
-        cssClass: "labelGroup",
-        radius: 4,
-        ports: [
-          {
-            id: draw2d.util.UUID.create(),
-            name: "labelPortLeft",
-            ...labelPort,
-            locatorAttr: {
-              x: 0,
-              y: 50,
-            },
-          },
-          {
-            id: draw2d.util.UUID.create(),
-            name: "labelPortRight",
-            ...labelPort,
-            locatorAttr: {
-              x: 100,
-              y: 50,
-            },
-          },
-        ],
-      },
-      {
-        type: "draw2d.shape.basic.Label",
-        id: componentId + "-title",
-        x: attr.x + 300 / 2 - 5,
-        y: attr.y + 80 / 4,
-        text: attr.title,
-        textAnchor: "middle",
-        stroke: 0,
-        fontSize: 20,
-        fontColor: "#4d5d80",
-        bgColor: "none",
-        selectable: false,
-        draggable: false,
-        cssClass: "titleLabel",
-        composite: componentId,
-      },
-    ];
-    return data;
-  };
-
-  var reader = new draw2d.io.json.Reader();
-
-  var root = rootJson({
-    x: 100,
-    y: 400,
-  });
-  var component1 = componentJson({
-    x: 360,
-    y: 280,
-    title: "Author",
-    bgColor: "#f0f0f0",
-    color: "#4d5d80",
-  });
-  var component2 = componentJson({
-    x: 360,
-    y: 580,
-    title: "Name",
-    bgColor: "#f0f0f0",
-    color: "#4d5d80",
-  });
-  var component3 = componentJson({
-    x: 720,
-    y: 180,
-    title: "Age",
-    bgColor: "#fd7e14",
-    color: "#ffffff",
-  });
-  var component4 = componentJson({
-    x: 720,
-    y: 380,
-    title: "Cover",
-    bgColor: "#fd7e14",
-    color: "#ffffff",
-  });
-  var data = root
-    .concat(component1)
-    .concat(component2)
-    .concat(component3)
-    .concat(component4);
-  reader.unmarshal(canvas, data);
-  $(document).on("keydown", function (e) {
-    var writer = new draw2d.io.json.Writer();
-    writer.marshal(canvas, function (json) {
-      console.log(JSON.stringify(json, null, 2));
-    });
-  });
+    
+});
+    
+    
 });
