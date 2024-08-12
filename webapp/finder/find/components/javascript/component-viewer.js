ab4083b03b1d2b10be1d3cd64b572a8229387bb5
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2024-08-12 23:57:50 +0600

    beutified onix

--- webapp/finder/find/components/javascript/component-viewer.js
+++ webapp/finder/find/components/javascript/component-viewer.js
@@ -43,7 +43,7 @@ $(document).on("draw2d", function () {
       var request = {
         componentdatasortby: "orderingUp",
         page: "1",
-        hitsperpage: "6",
+        hitsperpage: "5",
         query: {
           terms: [
             {
