9b5e2156858c9f66722f3eaa4d74a1f4461f1aae
Christopher Burkey <cburkey@entermediadb.org>
2024-09-26 05:38:04 -0600

    Dita render

--- webapp/finder/find/components/entities/metadata/import/ImportAny.groovy
+++ webapp/finder/find/components/entities/metadata/import/ImportAny.groovy
@@ -1,6 +1,6 @@
 import org.openedit.Data
 import org.openedit.data.Searcher
-import org.entermediadb.asset.importer.OnixImporter
+import org.entermediadb.asset.importer.*
 
 
 //Save to the right source path then use that Page path
@@ -15,3 +15,13 @@ if( type == "onix")
 	oniximporter.setEntity(context.getPageValue("entity"));
 	oniximporter.importData();
 }
+else if( type == "dita")
+	{
+		DitaImporter oniximporter = new DitaImporter();
+		oniximporter.setModuleManager(moduleManager);
+		oniximporter.setContext(context);
+		oniximporter.setMakeId(false);
+		oniximporter.setModule(module);
+		oniximporter.setEntity(context.getPageValue("entity"));
+		oniximporter.importData();
+	}
