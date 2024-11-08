b33435be7edcdab8d49061d137bbacc71073c163
Ian Miller <ian@netevolved.com>
2024-11-08 14:02:33 -0500

    metadata and templates, and scripts for LLM tagging

---
+++ webapp/finder/catalog/events/scripts/llm/analyzeassets.groovy
@@ -0,0 +1,81 @@
+package llm
+
+import org.entermediadb.asset.Asset
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.llm.GptManager
+import org.openedit.WebPageRequest
+import org.openedit.data.Searcher
+import org.openedit.hittracker.HitTracker
+import org.openedit.repository.ContentItem
+
+import com.google.gson.JsonObject
+
+import groovy.json.JsonSlurper
+
+public void tagAssets(){
+
+	//Create the MediaArchive object
+	WebPageRequest inReq = context;
+	MediaArchive archive = inReq.getPageValue("mediaarchive");
+	Searcher searcher = archive.getAssetSearcher();
+	//TODO:  get the bean to use programatically from catalog settings or something	
+	GptManager manager = archive.getBean("gptManager");
+	def cat = archive.getCategorySearcher().getRootCategory();
+	inReq.putPageValue("category", cat);
+	
+	//Refine this to use a hit tracker?
+	HitTracker assets = searcher.getAllHits();
+	String model = inReq.findValue("model.value");
+	if(model == null) {
+		model = archive.getCatalogSettingValue("gpt-model");
+	}
+
+	if(model==null) {
+		//model = "gpt-3.5-turbo-16k-0613";
+		model="gpt-4o-mini";
+	}
+
+	for (hit in assets) {
+		Asset asset = archive.getAsset(hit.id);
+		inReq.putPageValue("asset", asset);
+		
+		ContentItem item = archive.getGeneratedContent(asset, "image3000x3000.jpg");
+		if(item.exists()) {
+
+			InputStream inputStream = item.getInputStream()
+			String base64EncodedString = ''
+			try {
+				byte[] bytes = inputStream.bytes // Read InputStream as bytes
+				base64EncodedString = Base64.getEncoder().encodeToString(bytes) // Encode to Base64
+			} catch (Exception e) {
+				log.info("Error encoding asset to Base64: ${e}")
+			} finally {
+				inputStream.close() // Close the InputStream
+			}
+
+
+
+			String template = manager.loadInputFromTemplate(inReq, archive.getCatalogId() + "/gpt/templates/analyzeasset.html");
+			try{
+
+				JsonObject results = manager.callFunction(inReq, model, "generate_metadata", template, 0, 5000,base64EncodedString );
+				def jsonSlurper = new JsonSlurper()
+				def result = jsonSlurper.parseText(results.toString());
+				result.metadata.each { key, value ->
+					asset.setValue(key, value);
+				}
+				archive.saveAsset(asset);
+			}
+			catch(Exception e){
+				log.info(e);
+			}
+		}
+	}
+}
+
+tagAssets();
+
+
+
+
+
