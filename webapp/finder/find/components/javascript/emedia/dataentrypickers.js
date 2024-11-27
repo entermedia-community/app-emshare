5c73a5888dfde1065d3c931f7be6f91bf2c42043
Christopher Burkey <cburkey@entermediadb.org>
2024-11-27 09:57:19 -0500

    File picker

--- webapp/finder/find/components/javascript/emedia/dataentrypickers.js
+++ webapp/finder/find/components/javascript/emedia/dataentrypickers.js
@@ -271,10 +271,10 @@ lQuery(".pickerresults.pickerpickasset .resultsdivdata").livequery(
 		}
 		var row = $(clicked.closest(".resultsdivdata"));
 		var rowid = row.data("dataid");
-		var pickerresults = clicked.closest(".pickerresults");
+		var editdiv = clicked.closest(".editdiv");
 
-		if (pickerresults.length) {
-			var pickertarget = pickerresults.data("pickertargetfield");
+		if (editdiv.length) {
+			var pickertarget = editdiv.data("pickertargetfield");
 			pickertarget = $("#" + pickertarget); //This is the field itself
 			if (pickertarget.length > 0) {
 				var detailid = pickertarget.data("detailid");
