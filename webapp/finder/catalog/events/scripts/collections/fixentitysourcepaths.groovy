6169997f59d50eaf8e6600f4e8398913a50b832b
Christopher Burkey <cburkey@entermediadb.org>
2024-12-31 11:43:28 -0600

    Fix paths

---
+++ webapp/finder/catalog/events/scripts/collections/fixentitysourcepaths.groovy
@@ -0,0 +1,47 @@
+package collections;
+
+import org.entermediadb.asset.Category
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.projects.LibraryCollection
+import org.entermediadb.projects.ProjectManager
+import org.openedit.Data
+import org.openedit.data.Searcher
+import org.openedit.hittracker.HitTracker
+
+
+public void init(){
+
+	MediaArchive archive = context.getPageValue("mediaarchive");
+	Searcher collectionsearcher = archive.getSearcher("librarycollection");
+	HitTracker collections = collectionsearcher.getAllHits();
+	collections.enableBulkOperations();
+	int fixed = 0;
+	Collection tosave = new ArrayList();
+	collections.each
+	{
+		Data collection = (Data)it;
+		String catid = collection.getValue("rootcategory");
+		if( catid != null)
+		{
+			Category cat = archive.getCategory(catid);
+			if( cat == null)
+			{
+				log.error("No rootcategory on" + collection);
+				
+			}
+			collection.setValue("uploadsourcepath",cat.getCategoryPath());
+			tosave.add(collection);
+			fixed++;
+		}
+		else
+		{
+			log.error("No rootcategory on" + collection);
+		}
+		archive.saveData("librarycollection",tosave);
+	}
+	log.info("Fixed collections " + fixed);
+}
+
+
+init();
+
