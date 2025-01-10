67bc47651c5876a96821a0104e2c22019ba9a09a
Cristobal <cristobal.mejia@gmail.com>
2025-01-10 14:40:03 -0600

    Import Alfred Products from Piped DB

--- webapp/finder/catalog/events/scripts/librarycollection/importproductentities.groovy
+++ webapp/finder/catalog/events/scripts/librarycollection/importproductentities.groovy
@@ -1,14 +1,15 @@
 package librarycollection;
 
+import org.entermediadb.asset.Category
 import org.entermediadb.asset.MediaArchive
 import org.entermediadb.asset.util.CSVReader
+import org.entermediadb.modules.publishing.PubLookUp;
 import org.entermediadb.modules.update.Downloader
 import org.openedit.Data
 import org.openedit.OpenEditException
 import org.openedit.WebPageRequest
 import org.openedit.data.Searcher
 import org.openedit.page.Page
-
 
 public void init(){
 	System.out.println("import products");
@@ -30,45 +31,84 @@ public void init(){
 	Reader reader = upload.getReader();
 	try{
 		Integer li = 1;
-		CSVReader read = new CSVReader(reader, (char)'\t', true);
+		Integer foundcount = 0;
+		//CSVReader read = new CSVReader(reader, (char)'\t', true);
+		CSVReader read = new CSVReader(reader, (char)'|', true);
 		String[] headers = read.readNext();
 		String[] line;
 		while ((line = read.readNext()) != null){
-			String id = line[2];
-			//log.info("Line:"+li+" Id " + id);
-			Data product = searcher.searchById(id);
+			if (line.length < 2) 
+			{
+				continue;
+			}
+			String pubitem =  getline(line, 2); //Pub-Item
+			
+			if (pubitem == null) 
+			{
+				continue;
+			}
+			
+			Data product = searcher.searchById(pubitem);
 			if(product == null) {
 				product = searcher.createNewData();
 				
+				product.setId(pubitem);
 				
-				product.setName(id);
-				product.setId(id);
+				String productname =  getline(line, 9); //9 is Title
+				if (productname == null) {
+					productname = pubitem;
+				}
+				product.setName(productname);
+				
 				//collection.setValue("rootcategory",id);
 			}
-			product.setValue("library", "products");
-			if(line.length >=37) {
-				product.setValue("projectdescription", line[36]);
-				product.setValue("title", line[9]);
-				product.setValue("subtitle", line[10]);
-				product.setValue("genre", line[16]);
-				product.setValue("level", line[21]);
-				product.setValue("series", line[20]);
-				product.setValue("keywords", line[26]);
-			} else {
-				log.info("CSV Import, incomplete row line " + li +" id: "+id);
-			}
+			//product.setValue("library", "products");
 			
+			product.setValue("projectdescription", getline(line, 36));
+			
+			//product.setValue("title", line[9]);
+			product.setValue("subtitle",  getline(line, 10));
+			product.setValue("genre",  getline(line, 16));
+			product.setValue("level",  getline(line, 21));
+			product.setValue("series",  getline(line, 20));
+			product.setValue("keywords",  getline(line, 26));
+			product.setValue("pubstatus",  getline(line, 3));
+			
+			if (product.get("rootcategory") == null) {
+				PubLookUp publookup = archive.getBean("pubLookUp");
+				Category foundcat =  publookup.lookUpbyPubId(pubitem);
+				if (foundcat != null)
+				{
+					product.setValue("rootcategory", foundcat.getId());
+					//log.info("Line:"+li+" pubitem: " + pubitem + " category: " + foundcat.getParentCategories());
+					foundcount++;//found
+				}
+			}
 			rows.add(product);
-			li++;
-			if(rows.size() > 10000){
+			
+			if(rows.size() > 1000){
 				searcher.saveAllData(rows, null);
+				log.info("Saving "+rows.size()+" Products. "+foundcount+" categories found.")
 				rows.clear();
 			}
+			li++;
 		}
+		//log.info("Found: "+ foundcount + " on: " + li-1 + " lines");
 		searcher.saveAllData(rows, null);
+		log.info("Saving "+rows.size()+" Products. "+foundcount+" categories found.");
 	} catch (Exception e){
 		throw new OpenEditException(e);
 	}
 }
+
+public String getline(String[] line, Integer linenumber)
+{
+	if(line.length > linenumber) 
+	{
+		return line[linenumber];
+	}
+	return null;
+}
+
 init();
 
