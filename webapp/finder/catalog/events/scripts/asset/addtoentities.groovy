fa4ae17c7b8f1af659b17ad3791390a82955a1e7
Christopher Burkey <cburkey@entermediadb.org>
2025-02-03 10:06:50 -0600

    Add users

--- webapp/finder/catalog/events/scripts/asset/addtoentities.groovy
+++ webapp/finder/catalog/events/scripts/asset/addtoentities.groovy
@@ -5,24 +5,32 @@ import org.entermediadb.asset.MediaArchive
 import org.openedit.Data
 import org.openedit.data.Searcher
 import org.openedit.event.WebEvent
-public init(){
+public init()
+{
+	
+	String entitymoduleid = "entityproduct"; 
+	
     MediaArchive mediaArchive = (MediaArchive)context.getPageValue("mediaarchive");
     Collection hits = mediaarchive.query("asset").matches("productid","*").search();
-   
 	Collection tosave = new ArrayList();
     hits.each{
         Data hit = it;
         if(hit.productid){
-			Data product = mediaarchive.getCachedData("entityproduct",hit.productid).
+			Data product = mediaarchive.getCachedData("entitymoduleid",hit.productid).
 			Category cat = mediaarchive.getEntityManager().loadDefaultFolder(product,null);
-			if( !product.containsCategory(cat) )
+			if(cat != null && !product.isInCategory(cat) )
 			{
 				product.addCategory(cat);
 	            tosave.add(product);
+				if( tosave.size() == 1000 )
+				{
+					mediaArchive.saveData(entitymoduleid,tosave );
+					tosave.clear();
+				}
 			}
         }
-        
     }   
+	mediaArchive.saveData("entityproduct",tosave );
     
 }
 //init();
