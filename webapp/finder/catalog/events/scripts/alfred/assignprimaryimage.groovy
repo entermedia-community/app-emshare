534f607329e81ee2826d0a23b7c44aaf1d5473cc
Cristobal <cristobal.mejia@gmail.com>
2025-03-31 13:10:07 -0600

    assign primary image event

--- webapp/finder/catalog/events/scripts/alfred/assignprimaryimage.groovy
+++ webapp/finder/catalog/events/scripts/alfred/assignprimaryimage.groovy
@@ -2,18 +2,21 @@ package librarycollection;
 
 import org.entermediadb.asset.MediaArchive
 import org.openedit.Data
+import org.openedit.WebPageRequest
 import org.openedit.data.Searcher
 import org.openedit.hittracker.HitTracker
 
 public void init(){
 	
+	WebPageRequest req = context;
+
 	MediaArchive archive = req.getPageValue("mediaarchive");
 	Searcher publicationsearcher = archive.getSearcher("publication");
 	Searcher pubpartsearcher = archive.getSearcher("publicationpart");
 	Searcher pubscoresearcher = archive.getSearcher("publicationscore");
 	Searcher relatedsearcher = archive.getSearcher("publicationrelated");
 	
-
+	assignimage(archive, publicationsearcher);
 }
 
 public void assignimage(MediaArchive archive, Searcher searcher) {
@@ -21,29 +24,37 @@ public void assignimage(MediaArchive archive, Searcher searcher) {
 	HitTracker entities = searcher.query().and().missing("primaryimage").missing("primarymedia").search();
 	
 	if (entities != null) {
+		log.info("Missing " + entities.size() + " images - " + searcher.getSearchType());
 		entities.enableBulkOperations();
 		List tosave = new ArrayList();
 		entities.each{
 			Data entity = it;
-			//Find Product Image
-			if (entity.get("primaryimage") == null) {  //Overwrite if exists?
-				
-				//Assets in category:   793939  -- Publication Files/Catpics/Large
-				
-				String thumbnailcat = "793939";
-				 
-				Data asset = archive.getAssetSearcher().query().match("category", thumbnailcat).startsWith("name", entity.get("Ref_SKU")).searchOne();
-				if (asset != null)
-				{
-					entity.setValue("primaryimage", asset.getId());
-					tosave.add(entity);
-				}
+			if (entity.get("Ref_SKU") == null)
+			{
+				continue;
+			}
+			//Assets in category:   793939  -- Publication Files/Catpics/Large
+			
+			String thumbnailcat = "793939";
+			 
+			Data asset = archive.getAssetSearcher().query().match("category", thumbnailcat).startsWith("name", entity.get("Ref_SKU")).searchOne();
+			log.info("searching: " + asset);
+			if (asset != null)
+			{
+				entity.setValue("primaryimage", asset.getId());
+				log.info("Image found: " + asset);
+				tosave.add(entity);
 			}
 			if( tosave.size() == 1000)	{
 				searcher.saveAllData(tosave, null);
 				tosave.clear();
-				log.info("Saved: 1000 images");
+				log.info("Saved: " + tosave.size() + " images - " + searcher.getSearchType());
 			}
+		}
+		if( tosave.size() > 0 )	{
+			searcher.saveAllData(tosave, null);
+			tosave.clear();
+			log.info("Saved: " + tosave.size() + " images - " + searcher.getSearchType());
 		}
 	}
 }
