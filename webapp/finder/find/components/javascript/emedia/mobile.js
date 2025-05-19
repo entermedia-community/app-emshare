73e949bb1244c83edc8fed61aef59a0618fabed9
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-05-19 23:12:01 +0600

    more resopnsive stuff

---
+++ webapp/finder/find/components/javascript/emedia/mobile.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(window).on("resize", function () {
+    if ($(window).width() > 768) {
+      if (confirm("Switch to desktop view?")) {
+        window.location.reload();
+      }
+    }
+  });
+  lQuery(".entity-tab.current-entity").livequery(function (e) {
+    $(this).scrollLeft(0);
+  });
+});
