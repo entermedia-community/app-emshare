b15b0d08a8724ef531ba36b3867b2909f546cb27
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-05-24 03:41:16 +0600

    login page fix

--- webapp/finder/find/components/javascript/emedia/mobile.js
+++ webapp/finder/find/components/javascript/emedia/mobile.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $(window).on("resize", function () {
-    if ($(window).width() > 768) {
-      if (confirm("Switch to desktop view?")) {
-        window.location.reload();
-      }
-    }
-  });
+  // $(window).on("resize", function () {
+  //   if ($(window).width() > 768) {
+  //     if (confirm("Switch to desktop view?")) {
+  //       window.location.reload();
+  //     }
+  //   }
+  // });
   lQuery(".entity-tab.current-entity").livequery(function (e) {
     $(this).scrollLeft(0);
   });
