556a64341304e1a37a58153a0e5b449e026b2ea0
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-05-26 21:55:14 +0600

    Enhance mobile view handling and layout adjustments
    - Updated resize event handling in mobile.js to improve responsiveness.
    - Modified includes.html to load mobile styles without media query.
    - Added new mobile gallery and brick vertical templates for better asset display.
    - Improved CSS for mobile elements, ensuring better layout and visibility.
    - Refactored result views to accommodate mobile-specific rendering.

--- webapp/finder/find/components/javascript/emedia/mobile.js
+++ webapp/finder/find/components/javascript/emedia/mobile.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-  // $(window).on("resize", function () {
-  //   if ($(window).width() > 768) {
-  //     if (confirm("Switch to desktop view?")) {
-  //       window.location.reload();
-  //     }
-  //   }
-  // });
+  $(window).on("resize", function () {
+    var userAgent = navigator.userAgent;
+    window.isNarrow =
+      $(window).width() <= 768 || /Android|iPhone|iPad|iPod/.test(userAgent);
+    if (!isNarrow) {
+      if (confirm("Switch to desktop view?")) {
+        window.location.reload();
+      }
+    }
+  });
   lQuery(".entity-tab.current-entity").livequery(function (e) {
     $(this).scrollLeft(0);
   });
