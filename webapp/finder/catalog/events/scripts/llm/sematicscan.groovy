b8fb33218dbbc632d51a420893aa2dab274329a3
Cristobal <cristobal.mejia@gmail.com>
2025-09-19 16:47:28 -0600

    not needed import

--- webapp/finder/catalog/events/scripts/llm/sematicscan.groovy
+++ webapp/finder/catalog/events/scripts/llm/sematicscan.groovy
@@ -2,8 +2,6 @@ package asset
 
 import org.entermediadb.ai.classify.SemanticFieldManager
 import org.entermediadb.asset.*
-import org.openedit.locks.Lock
-
 public void init()
 {
 	MediaArchive archive = context.getPageValue("mediaarchive");//Search for all files looking for videos
