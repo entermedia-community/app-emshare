92f5edb748a620d687d4c682fc4a37d9c92ddfba
Christopher Burkey <cburkey@entermediadb.org>
2025-12-03 16:49:37 -0600

    Server sort

--- webapp/finder/catalog/events/scripts/llm/updateaiservers.groovy
+++ webapp/finder/catalog/events/scripts/llm/updateaiservers.groovy
@@ -1,16 +1,10 @@
-import org.entermediadb.ai.llm.BasicLlmResponse
 import org.entermediadb.ai.llm.LlmConnection
 import org.entermediadb.ai.llm.LlmResponse
 import org.entermediadb.asset.MediaArchive
+import org.entermediadb.net.HttpSharedConnection
 import org.json.simple.JSONArray
 import org.json.simple.JSONObject
 import org.openedit.Data
-import org.openedit.MultiValued
-import org.openedit.data.QueryBuilder
-import org.openedit.data.Searcher
-import org.openedit.hittracker.HitTracker
-
-import netscape.javascript.JSObject
 
 public void init() {
 	
@@ -21,11 +15,19 @@ public void init() {
 	Collection instances = response.getRawResponse().get("instances");
 	
 	Collection visionservers = new ArrayList();
+//	String[] m50v = new String[2];
+//	m50v[0] = "https://llamam50vision.entermediadb.net";
+//	visionservers.add(m50v);
+	
 	Collection toolsservers = new ArrayList();
-
+	
+	String[] m50t = new String[2];
+	m50t[0] = "https://llamam50.entermediadb.net";
+	toolsservers.add(m50t);
+	
 	for (instance in instances) {
 		//log.info(instance);
-		String ippaddr = instance.get("public_ipaddr");
+		String ippaddr = "http://" + instance.get("public_ipaddr");
 		JSONObject ports = instance.get("ports");
 		if (ports != null && instance.get("cur_state").equals("running"))
 		{
@@ -53,29 +55,71 @@ public void init() {
 		}
 	}
 	
-	
-	
-	//Todo: Sort servers by performance
-	updateServer(visionservers, "llamaVisionConnection");
-	updateServer(toolsservers, "llamaOpenAiConnection");
+	String[] fastestvision = sortbyspeed(visionservers);
+	updateServer(fastestvision, "llamaVisionConnection");
+
+	String[] fastesttool = sortbyspeed(toolsservers);
+	updateServer(fastesttool, "llamaOpenAiConnection");
 
 }
 
+protected String[] sortbyspeed(Collection<String[]> servers) 
+{
+	
+	Map speeds = new HashMap();
+	
+	for(String[] server in servers)
+	{
+		String address = server[0];
+		String serverport = server[1];
+		if(serverport != null)
+		{
+			address = address + ":" + serverport;
+		}
+		HttpSharedConnection connection = new HttpSharedConnection();
+		long start = System.currentTimeMillis();
+		try
+		{
+			String got = connection.getResponseString(address);
+			if( got != null )
+			{
+				long end =  System.currentTimeMillis();
+				long diff = end - start;
+				speeds.put(diff, server);
+				log.info(address + " ran in " + diff + " milliseconds");
+			}
+		}
+		catch( Exception ex)
+		{
+			log.info(address + " had error " + ex);
+			//Ignore
+		}
+	}
+	List<Long> speeds = new ArrayList(speeds.keySet());
+	speeds.sort(Comparator.naturalOrder());
+	
+	long lowest = speeds.get(0);
+	String[] fast = speeds.get(lowest);
+	return fast;
+	
+}
 
-public void updateServer(Collection servers, String bean)
+public void updateServer(String[] fastest, String bean)
 {
 	MediaArchive archive = context.getPageValue("mediaarchive");
 	if (!servers.isEmpty())
 	{
-		String[] fastest = servers.iterator().next();
 		String serverip = fastest[0];
-		
 		String serverport = fastest[1];
 		//llamaVisionConnection
 		ArrayList tosave = new ArrayList();
 		Collection<Data> currentservers = archive.query("aiserver").exact("connectionbean", bean).search();
 		for (Data server in currentservers) {
-			String address = "http://" + serverip + ":" + serverport;
+			String address = serverip;
+			if(serverport != null)
+			{
+				address = address + ":" + serverport;
+			}
 			if (!address.equals(server.get("serverroot")))
 			{
 				server.setValue("serverroot", address);
