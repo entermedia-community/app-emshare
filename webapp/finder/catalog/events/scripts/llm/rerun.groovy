b43230d4fe320e2562c7c6eb141bdd1c81055a5a
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-12-27 18:49:13 +0600

    Rename 'entityembedded' to 'entityembeddingstatus' for consistency and update related query logic in rerun.groovy

--- webapp/finder/catalog/events/scripts/llm/rerun.groovy
+++ webapp/finder/catalog/events/scripts/llm/rerun.groovy
@@ -14,7 +14,7 @@ public void init() {
 		//QueryBuilder query = archive.query(module.getId()).exact("taggedbyllm", "true");
 		
 		//Re-run Document Ebedded only
-		QueryBuilder query = archive.query(module.getId()).exists("entityembedded");
+		QueryBuilder query = archive.query(module.getId()).exists("entityembeddingstatus");
 
 		//QueryBuilder query = archive.query(module.getId()).exact("taggedbyllm", "true").exists("semantictopics");
 		
@@ -31,7 +31,7 @@ public void init() {
 			Data row = searcher.loadData(data);
 			
 			row.setValue("taggedbyllm", false);
-			row.setValue("entityembedded", false);
+			row.setValue("entityembeddingstatus", "notembedded"); // TODO: delete previous embedding
 			row.setValue("pagescreatedfor", null);
 			
 	//		row.setValue("llmerror", false);
