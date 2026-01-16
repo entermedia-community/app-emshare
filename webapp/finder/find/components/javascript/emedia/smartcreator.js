4d2f1aad9ea06b97885c369402db25af3a273009
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-01-16 06:18:34 +0600

    feat: Add Smart Creator and Chatterbox components
    - Introduced new Chatterbox configuration files (_site.xconf, index.xconf) to manage chat functionalities.
    - Created HTML templates for Chatterbox including edit area, hints, and message display.
    - Developed the main Smart Creator interface with sections for content creation and chat integration.
    - Implemented action buttons for section management (add, edit, delete) in the section templates.
    - Enhanced styles for the new components to ensure a cohesive user experience.
    - Updated links in the tutorial suggestions to redirect to the new Smart Creator interface.
    - Added backend configurations for the Smart Creator module in the media database.

---
+++ webapp/finder/find/components/javascript/emedia/smartcreator.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+	var applink =
+		$("#application").data("siteroot") + $("#application").data("apphome");
+
+	lQuery(".creator-maker").livequery("click", function (e) {
+		var editor = $(this).find(".editable-content.ck");
+		if (editor.length > 0) {
+			var clickedElement = $(e.target);
+			if (
+				!clickedElement.closest(".editable-content.ck").length &&
+				!clickedElement.closest(".ck-toolbar").length
+			) {
+				e.preventDefault();
+				e.stopImmediatePropagation();
+				customToast(
+					"You have unsaved changes. Please save or cancel your current edit.",
+					{ id: "alert", positive: false }
+				);
+				editor.focus();
+				return false;
+			}
+		}
+	});
+
+	lQuery(".action-btn").livequery("click", function (e) {
+		e.preventDefault();
+		e.stopImmediatePropagation();
+		var action = $(this).data("action");
+		console.log(action);
+		if (action === "edit") {
+			var editor = $(this)
+				.closest(".creator-section-content")
+				.find(".editable-content");
+			editor.data("imagepickerhidden", "true");
+			$(window).trigger("inlinehtmlstart", [editor]);
+		} else if (action === "move-up" || action === "move-down") {
+			var section = $(this).closest(".creator-section-content");
+			if (!section.length > 0) {
+				section = $(this).closest(".creator-section");
+			}
+			if (action === "move-up") {
+				section.prev().before(section);
+			} else {
+				section.next().after(section);
+			}
+
+			// TODO: Update the section order in the backend
+		}
+	});
+	var $a = $(
+		'<a href="/site/find/components/smartcreator/index.html" class="emdialog" data-oemaxlevel="1" data-tutorialid="AZu-haHOtKTCl_ZN-pzS"></a>'
+	);
+	$(document.body).append($a);
+	$a.trigger("click");
+
+	$("#closecreator").on("click", function () {
+		console.log("object");
+		closeemdialog($(this).closest(".modal"));
+	});
+});
