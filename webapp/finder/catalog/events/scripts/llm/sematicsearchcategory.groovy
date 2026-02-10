d628c235fa43f3eb04d38708840f1f03e19df552
Christopher Burkey <cburkey@entermediadb.org>
2026-02-10 15:08:14 -0600

    New API

--- webapp/finder/catalog/events/scripts/llm/sematicsearchcategory.groovy
+++ webapp/finder/catalog/events/scripts/llm/sematicsearchcategory.groovy
@@ -1,15 +1,15 @@
 package asset
 
-import org.entermediadb.ai.assistant.AssistantManager
+import org.entermediadb.ai.assistant.SearchingManager
 import org.entermediadb.asset.*
 
 public void init()
 {
 	MediaArchive archive = context.getPageValue("mediaarchive");//Search for all files looking for videos
 
-	AssistantManager manager = archive.getBean("assistantManager");
+	SearchingManager manager = archive.getBean("searchingManager");
 	
-	manager.rescanSearchCategories();
+	manager.rescanSearchCategories(log);
 		
 }
 
