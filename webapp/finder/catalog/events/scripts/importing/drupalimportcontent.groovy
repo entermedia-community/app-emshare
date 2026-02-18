7086fd61e1dc1fe9ef5be7703ec9684df89abaf6
Cristobal <cristobal.mejia@gmail.com>
2026-02-18 12:58:26 -0600

    Drupal tables

--- webapp/finder/catalog/events/scripts/importing/drupalimportcontent.groovy
+++ webapp/finder/catalog/events/scripts/importing/drupalimportcontent.groovy
@@ -6,6 +6,9 @@ import org.entermediadb.drupal.DrupalManager
 public void init() {
 	MediaArchive archive = context.getPageValue("mediaarchive");
 	
+	//delete all articles first
+	archive.getSearcher("article").deleteAll(null);
+	
 	
 	DrupalManager manager = archive.getModuleManager().getBean( "DrupalManager");
 	manager.setCatalogId(archive.getCatalogId());
